5afe0e59803253717b8597d9e3cf22ecba1476e3
Xabier Martinez <xmartinez1702@gmail.com>
2021-08-08 16:23:08 +0200

    Move state to remote

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = "~> 1.0"
 
+  backend "s3" {
+    bucket = "myapp-bucket"
+    key    = "myapp/state.tfstate"
+    region = "eu-west-1"
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,7 +17,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "eu-west-1"
+  region  = "eu-west-1"
   profile = "xabi"
 }
 
@@ -20,8 +27,8 @@ module "vpc" {
   name = "my-vpc"
   cidr = var.vpc_cdir_block
 
-  azs             = [var.avail_zone]
-  public_subnets  = [var.subnet_cidr_block]
+  azs                = [var.avail_zone]
+  public_subnets     = [var.subnet_cidr_block]
   public_subnet_tags = { Name = "${var.env_prefix}-subnet-1" }
 
   tags = {
@@ -33,12 +40,12 @@ module "vpc" {
 module "myapp-server" {
   source = "./modules/webserver"
 
-  vpc_id = module.vpc.vpc_id
-  my_ip = var.my_ip
-  subnet_id = module.vpc.public_subnets[0]
-  image_name = var.image_name
+  vpc_id              = module.vpc.vpc_id
+  my_ip               = var.my_ip
+  subnet_id           = module.vpc.public_subnets[0]
+  image_name          = var.image_name
   public_key_location = var.public_key_location
-  instance_type = var.instance_type
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
+  instance_type       = var.instance_type
+  avail_zone          = var.avail_zone
+  env_prefix          = var.env_prefix
 }
